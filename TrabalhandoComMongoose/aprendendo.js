46017d232df21fa8b1ceabd8b936b7396741e63e
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com>
2024-01-24 18:13:15 -0300

    Tentativa de criar uma collection no mongoDB usando mongoose porém o curso que estava vendo é desatualizado kkk 5 anos atrás é foda

--- TrabalhandoComMongoose/aprendendo.js
+++ TrabalhandoComMongoose/aprendendo.js
@@ -1,14 +1,45 @@
-// como se conectar ao banco de dados mongo
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-
-// Config mongoose
 mongoose.Promise = global.Promise;
-main().catch(err => console.log(err));
 
-// a conexão é passado através de uma especie de URL (É POSSÍVEL CRIAR O BANCO DAQUI MESMO) o /aprendendo foi criado agora no visual
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
-  console.log("Deu certo")
-}
+mongoose.connect("mongodb://localhost:27017/teste", {
+  useNewUrlParser: true,
+}).then(() => {
+  console.log('conectado');
+}).catch((err) => {
+  console.log('deu erro' + err);
+});
+
+const Schema = mongoose.Schema; // Add this line to define the Schema object
+
+const UsuarioSchema = new Schema({
+  nome: {
+    type: String,
+    require: true
+  },
+  sobrenome: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  idade: {
+    type: Number,
+    require: true
+  }
+});
+
+const Usuario = mongoose.model('usuarios', UsuarioSchema); // Create a model using the schema
+
+new Usuario({
+  nome: "Guilherme",
+  sobrenome: "Lima", // Corrected "Sobrenome" to "sobrenome"
+  email: "Exemplo@gmail.com",
+  idade: 18
+}).save().then(() => {
+  console.log("usuario criado com sucesso");
+}).catch((err) => {
+  console.log("Houve erro ao criar o usuario" + err);
+});
